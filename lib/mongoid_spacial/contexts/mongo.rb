2bf6171443a7b9de3c2d4856059e42b6e31d4451
Ryan Ong <ryanong@gmail.com>
2011-06-23 01:17:45 -0400

    now with pagination, need to add some tests

--- lib/mongoid_spacial/contexts/mongo.rb
+++ lib/mongoid_spacial/contexts/mongo.rb
@@ -2,21 +2,21 @@
 module Mongoid #:nodoc:
   module Contexts #:nodoc:
     class Mongo #:nodoc:
-      
+
       # Fetches rows from the data base sorted by distance.
       # In MongoDB versions 1.7 and above it returns a distance.
       # Uses all criteria chains except without, only, asc, desc, order_by
       #
       # @example Minimal Query
-      #   
+      #
       #   Address.geo_near([70,40])
       #
       # @example Chained Query
-      #   
+      #
       #   Address.where(:state => 'ny').geo_near([70,40])
       #
       # @example Calc Distances Query
-      #   
+      #
       #   Address.geo_near([70,40], :max_distance => 5, :unit => 5)
       #
       # @param [ Array, Hash, #to_lng_lat ] center The center of where to calculate distance from
@@ -31,57 +31,29 @@ module Mongoid #:nodoc:
       #
       # @return [ Array ] Sorted Rows
       def geo_near(center, opts = {})
+        opts = self.options.merge(opts)
+        # convert point
         center = center.to_lng_lat if center.respond_to?(:to_lng_lat)
 
+        # set default opts
         if distance_multiplier = Mongoid::Spacial.earth_radius[opts.delete(:unit)]
           opts[:distance_multiplier] = distance_multiplier
         end
 
+        if opts.has_key?(:page)
+          opts[:page] ||= 1
+          opts[:paginator] ||= Mongoid::Spacial.paginator
+            if opts[:paginator] == :will_paginate
+              opts[:per_page] ||= klass.per_page
+            elsif opts[:paginator] == :kaminari
+              opts[:per_page] ||= Kaminari.config.default_per_page
+            else
+              opts[:per_page] ||= 25
+            end
+        end
         query = create_geo_near_query(center,opts)
         results = klass.db.command(query)
-        if results['results'].kind_of?(Array) && results['results'].size > 0
-          rows = results['results'].collect do |result|
-            res = Mongoid::Factory.from_db(klass, result.delete('obj'))
-            res.geo = {}
-            # camel case is awkward in ruby when using variables...
-            if result['dis']
-              res.geo[:distance] = result.delete('dis').to_f
-            end
-            result.each do |key,value|
-              res.geo[key.snakecase.to_sym] = value
-            end
-            # dist_options[:formula] = opts[:formula] if opts[:formula]
-            opts[:calculate] = klass.spacial_fields_indexed if klass.spacial_fields_indexed.kind_of?(Array) && opts[:calculate] == true
-            if opts[:calculate]
-              opts[:calculate] = [opts[:calculate]] unless opts[:calculate].kind_of? Array
-              opts[:calculate] = opts[:calculate].map(&:to_sym) & geo_fields
-              if klass.spacial_fields_indexed.kind_of?(Array) && klass.spacial_fields_indexed.size == 1
-                primary = klass.spacial_fields_indexed.first
-              end
-              opts[:calculate].each do |key|
-                key = (key.to_s+'_distance').to_s
-                res.geo[key] = res.distance_from(key,center, opts[:distance_multiplier])
-                res.geo[:distance] = res.geo[key] if primary && key == primary
-              end
-            end
-            res
-          end
-        else
-          rows = []
-        end
-        # if opts.has_key?(:page) || opts[:paginator]
-        #   opts[:paginator] ||= Mongoid::Spacial::Config.paginator
-        #   if opts[:paginator] == :kaminari
-
-        #   elsif opts[:paginator] == :will_paginate
-        #   end
-        # end
-
-        if self.options[:skip] && rows.size > self.options[:skip]
-          rows[self.options[:skip]..rows.size-1]
-        else
-          rows
-        end
+        Mongoid::Spacial::GeoNear.new(klass,results,opts)
       end
 
       private
@@ -90,14 +62,16 @@ module Mongoid #:nodoc:
         # minimum query
         query = {
           :geoNear  => klass.to_s.tableize,
-          :near     => center, 
+          :near     => center,
         }
 
         # create limit and use skip
-        if opts[:num] 
-          query['num']         = (self.options[:skip] || 0) + opts[:num].to_i          
-        elsif self.options[:limit]
-          query['num']         = (self.options[:skip] || 0) + self.options[:limit]
+        if opts[:num]
+          query['num']         = (opts[:skip] || 0) + opts[:num].to_i
+        elsif opts[:limit]
+          query['num']         = (opts[:skip] || 0) + opts[:limit]
+        elsif opts[:page]
+          query['num'] = (opts[:page] * opts[:per_page])
         end
 
         # allow the use of complex werieis
@@ -112,7 +86,7 @@ module Mongoid #:nodoc:
           query['maxDistance'] = query['maxDistance']/opts[:distance_multiplier] if opts[:distance_multiplier]
         end
 
-        if klass.db.connection.server_version >= '1.7'          
+        if klass.db.connection.server_version >= '1.7'
           query['spherical']  = true if opts[:spherical]
 
           # mongodb < 1.7 returns degrees but with earth flat. in Mongodb 1.7 you can set sphere and let mongodb calculate the distance in Miles or KM
